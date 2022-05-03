46ed6df7994081884f2823071c8c1f4148445d31
amazingCoding <sanylanv@gmail.com>
2022-05-03 19:42:44 +0800

    test ios

--- ios/SlideAreasModule.m
+++ ios/SlideAreasModule.m
@@ -2,18 +2,4 @@
 
 @implementation SlideAreasModule
 
-RCT_EXPORT_MODULE()
-
-// Example method
-// See // https://reactnative.dev/docs/native-modules-ios
-RCT_REMAP_METHOD(multiply,
-                 multiplyWithA:(nonnull NSNumber*)a withB:(nonnull NSNumber*)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
-{
-  NSNumber *result = @([a floatValue] * [b floatValue]);
-
-  resolve(result);
-}
-
 @end
